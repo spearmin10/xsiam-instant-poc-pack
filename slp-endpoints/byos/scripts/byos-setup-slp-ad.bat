fac469df2d2d67dd71a15f0655b8741905d6c0a7
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2026-01-26 16:36:37 +0900

    Add files via upload

--- slp-endpoints/byos/scripts/byos-setup-slp-ad.bat
+++ slp-endpoints/byos/scripts/byos-setup-slp-ad.bat
@@ -1,8 +1,8 @@
+@echo off
 pushd "%~dp0"
 
 set SCRIPT_NAME=byos-setup-slp-ad-default.ps1
-set SCRIPT_URL=https://github.com/spearmin10/xsiam-instant-poc-pack/blob/main/slp-endpoints/byos/scripts/%SCRIPT_NAME%?raw=true
-set 
+set SCRIPT_URL="https://github.com/spearmin10/xsiam-instant-poc-pack/blob/main/slp-endpoints/byos/scripts/%SCRIPT_NAME%?raw=true"
 
 curl -Lo "%SCRIPT_NAME%" -H "Cache-Control: no-cache, no-store" "%SCRIPT_URL%" 2> NUL
 if errorlevel 1 (
