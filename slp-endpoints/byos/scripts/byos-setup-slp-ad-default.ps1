990aa621b8822bfa99d0c2fea4240fae6cc3eb29
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2026-01-26 17:20:42 +0900

    Add files via upload

--- slp-endpoints/byos/scripts/byos-setup-slp-ad-default.ps1
+++ slp-endpoints/byos/scripts/byos-setup-slp-ad-default.ps1
@@ -20,18 +20,46 @@ function ReadPassword (
     }
 }
 
+function IsDirectory (
+    [string]$path
+) {
+    return ![string]::IsNullOrEmpty($path) -And (Test-Path $path) -And ((Get-Item $path) -is [IO.DirectoryInfo])
+}
+
+function DownloadFile (
+    [string]$url,
+    [string]$save_as
+) {
+    $cli = New-Object Net.WebClient
+    $cli.Headers.Add("Cache-Control", "no-cache, no-store")
+
+    if ([string]::IsNullOrEmpty($save_as) -Or (IsDirectory $save_as)) {
+        $uri = New-Object System.Uri($url)
+        if (!$uri.AbsolutePath.EndsWith("/")) {
+            $filename = Split-Path $uri.AbsolutePath -Leaf
+        } else {
+            $filename = "temp.dat"
+        }
+        if ((IsDirectory $save_as)) {
+            $save_as = [IO.Path]::GetFullPath((Join-Path $save_as $filename))
+        } else {
+            $save_as = $filename
+        }
+    }
+    $cli.DownloadFile($url, $save_as)
+    return (Get-Item $save_as).FullName
+}
+
 $user_id = "lab-user"
 $password = ReadPassword "Input password for ${user_id}" "Paloalto1!"
+$script_name = "byos-setup-slp-ad.ps1"
+$script_path = DownloadFile "https://github.com/spearmin10/xsiam-instant-poc-pack/blob/main/slp-endpoints/byos/scripts/${script_name}?raw=true" $script_name
 
-$script = ([ScriptBlock]::Create((New-Object Net.WebClient).DownloadString(
-  "https://github.com/spearmin10/xsiam-instant-poc-pack/blob/main/slp-endpoints/byos/scripts/byos-setup-slp-ad.ps1?raw=true"
-)))
-$script.Invoke(
-  "-ComputerIP", "172.16.77.240",
-  "-ComputerName", "SLP-AD",
-  "-ComputerDnsServers", "1.1.1.1,8.8.8.8",
-  "-LocalUserID", $user_id,
-  "-LocalPassword", $password,
-  "-DomainDnsName", "corp.cortex.lan",
-  "-DomainOU", "CORTEX"
-)
+& $script_path `
+  -ComputerIP 172.16.77.240 `
+  -ComputerName SLP-AD `
+  -ComputerDnsServers "1.1.1.1,8.8.8.8" `
+  -LocalUserID $user_id `
+  -LocalPassword $password `
+  -DomainDnsName corp.cortex.lan `
+  -DomainOU CORTEX
